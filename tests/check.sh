25e689b2c858dd6a02fef6921f14b117e8ff98d5
Takahiro Ueda <tueda@st.seikei.ac.jp>
2018-07-13 16:01:34 +0900

    [test] Colorize tee'ed test output

--- tests/check.sh
+++ tests/check.sh
@@ -6,21 +6,23 @@ check_dir() {
   (
     cd "$1"
     make clean
-    make | tee make.out
+    make MAKE_COLORS=always | tee make.out
   )
-
-  [ $# -le 1 ] && return
 
   if grep 'Rerun' $1/*.log | grep -v 'Package: rerunfilecheck\|rerunfilecheck.sty'; then
     echo "FAIL: documents incomplete"
-    exit 1;
+    exit 1
   fi
+
+  [ $# -le 1 ] && return
 
   num=$(grep halt-on-error "$1/make.out" | wc -l)
   if [ $2 -ne $num ]; then
     echo "FAIL: wrong number of running LaTeX: $2 (must be $num)" >&2
     exit 1
   fi
+
+  rm "$1/make.out"
 }
 
 check_dir basic_latex 5
