b30d8ad0627177434c6116a73de07136d49c354f
Takahiro Ueda <tueda@st.seikei.ac.jp>
2018-08-06 22:25:44 +0900

    Adopt Bats for the testing framework

---
+++ tests/check.bats
@@ -0,0 +1,45 @@
+test_dir() {(
+  set -eu
+  set -o pipefail
+
+  (
+    cd "$1"
+    make clean
+    make | tee make.out
+  )
+
+  if grep 'Rerun' $1/*.log | grep -v 'Package: rerunfilecheck\|rerunfilecheck.sty'; then
+    echo "FAIL: documents incomplete"
+    exit 1
+  fi
+
+  [ $# -le 1 ] && return
+
+  num=$(grep halt-on-error "$1/make.out" | wc -l)
+  if [ $2 -ne $num ]; then
+    echo "FAIL: wrong number of running LaTeX: $num (must be $2)" >&2
+    exit 1
+  fi
+
+  rm "$1/make.out"
+)}
+
+@test "latex" {
+  test_dir latex 5
+}
+
+@test "bibtex" {
+  test_dir bibtex 3
+}
+
+@test "makeindex" {
+  test_dir makeindex 2
+}
+
+@test "makeglossaries" {
+  test_dir makeglossaries 2
+}
+
+@test "axohelp" {
+  test_dir axohelp 2
+}
--- tests/check.sh
+++ tests/check.sh
@@ -1,33 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 set -eu
-set -o pipefail
-
-check_dir() {
-  (
-    cd "$1"
-    make MAKE_COLORS=always clean
-    make MAKE_COLORS=always | tee make.out
-  )
-
-  if grep 'Rerun' $1/*.log | grep -v 'Package: rerunfilecheck\|rerunfilecheck.sty'; then
-    echo "FAIL: documents incomplete"
-    exit 1
-  fi
-
-  [ $# -le 1 ] && return
-
-  num=$(grep halt-on-error "$1/make.out" | wc -l)
-  if [ $2 -ne $num ]; then
-    echo "FAIL: wrong number of running LaTeX: $num (must be $2)" >&2
-    exit 1
-  fi
-
-  rm "$1/make.out"
-}
-
-check_dir latex 5
-check_dir bibtex 3
-check_dir makeindex 2
-check_dir makeglossaries 2
-check_dir axohelp 2
-echo "OK: all tests passed."
+if [ ! -x ./bats/bin/bats ]; then
+  git -C .. submodule update --init tests/bats
+fi
+./bats/bin/bats *.bats
