b62be01731828fc57874ed812e2b531b650e6609
Takahiro Ueda <tueda@st.seikei.ac.jp>
2020-09-03 21:40:06 +0900

    ci(travis): stop using make check
    This avoids weird "make check" behaviour on Linux in Travis CI:
    it exits with 0 on failure. (It was OK on macOS, failed as expected.)

--- tests/check.sh
+++ tests/check.sh
@@ -1,6 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
+set -o pipefail
+
+cd $(cd $(dirname $BASH_SOURCE); pwd)
+
 if [ ! -x ./bats/bin/bats ]; then
   git -C .. submodule update --init tests/bats
 fi
+
 ./bats/bin/bats "$@" *.bats
