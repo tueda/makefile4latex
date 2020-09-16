daf2ce0d8b2b1c5159402867d94453ea33ea1584
Takahiro Ueda <tueda@st.seikei.ac.jp>
2020-09-16 21:43:55 +0900

    build: set up pre-commit hooks

--- latex.mk
+++ latex.mk
@@ -1,3 +1,9 @@
+# Check if the repository is dirty.
 check-dirty:
 	@git status
 	@[ -z "$$(git status --porcelain)" ]
+
+# Install pre-commit hooks.
+install:
+	@pre-commit install
+	@pre-commit install --hook-type commit-msg
