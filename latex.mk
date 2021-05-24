fd6a50fa9a27e97c5bfed75c0e30a9f08d12e2f9
Takahiro Ueda <tueda@st.seikei.ac.jp>
2021-05-24 23:14:59 +0900

    ci: test with GitHub Actions

--- latex.mk
+++ latex.mk
@@ -7,3 +7,20 @@ check-dirty:
 install:
 	@pre-commit install
 	@pre-commit install --hook-type commit-msg
+
+# For CI.
+ci-test:
+	MAKE_COLORS=always MAKEFLAGS='' ./tests/check.sh -T
+	$(MAKE) check-dirty
+
+ci-test-builddir:
+	MAKE_COLORS=always MAKEFLAGS='BUILDDIR=.build' ./tests/check.sh -T
+	$(MAKE) check-dirty
+
+ci-test-dev:
+	MAKE_COLORS=always MAKEFLAGS='DEV=1' ./tests/check.sh -T
+	$(MAKE) check-dirty
+
+ci-test-dev-builddir:
+	MAKE_COLORS=always MAKEFLAGS='DEV=1 BUILDDIR=.build' ./tests/check.sh -T
+	$(MAKE) check-dirty
